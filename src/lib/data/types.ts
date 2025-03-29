deceb7fdf4d747db3dcdb39748e7a4cac5cc6c6f
lumintriet <lumintriet@gmail.com>
2025-03-29 09:50:15 +0000

    Data types and fake db.

---
+++ src/lib/data/types.ts
@@ -0,0 +1,22 @@
+type MessageType = {
+  id: string;
+  role: "user" | "assistant";
+  content: string;
+  name?: string;
+  createdAt: string;
+  chatId: string;
+};
+
+type ChatType = {
+  id: string;
+  title: string;
+  createdAt: string;
+  lastOpenedAt: string;
+};
+
+type FakeDbType = {
+  chats: ChatType[];
+  messages: MessageType[];
+};
+
+export type { MessageType, ChatType, FakeDbType };
