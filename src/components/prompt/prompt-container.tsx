80a5296c93cd388bd5da0bb802b6b5b1c64fb943
lumintriet <lumintriet@gmail.com>
2025-03-30 13:43:05 +0000

    Handle send prompt in global state.

--- src/components/prompt/prompt-container.tsx
+++ src/components/prompt/prompt-container.tsx
@@ -1,9 +1,33 @@
-import { promptStore$ } from "@/lib/data/stores";
+import { chatStore$, promptStore$ } from "@/lib/data/stores";
 import { $React } from "@legendapp/state/react-web";
 import { Button } from "../ui/button";
 import { Send } from "lucide-react";
+import { use$ } from "@legendapp/state/react";
+import { createNewChat, createNewMessage } from "@/lib/data/create-new";
 
 function PromptContainer() {
+  const prompt = use$(promptStore$.prompt);
+  const activeChat = use$(chatStore$.activeChat);
+  function handleSend() {
+    if (prompt) {
+      let chatId: string;
+      if (!activeChat) {
+        const newChat = createNewChat();
+        chatStore$.activeChat.set(newChat);
+        chatId = newChat.id;
+      } else {
+        chatId = activeChat.id;
+      }
+      const newUserMessage = createNewMessage({
+        role: "user",
+        content: prompt,
+        activeChatId: chatId,
+      });
+
+      chatStore$.activeMessages.push(newUserMessage);
+      promptStore$.prompt.set("");
+    }
+  }
   return (
     <div className="sticky bottom-0 flex max-h-3/4 w-full flex-col gap-y-3 rounded-t-lg bg-sidebar px-3 py-2">
       <$React.textarea
@@ -14,7 +38,7 @@ function PromptContainer() {
       />
       <div className="flex w-full justify-between">
         <Button>Gemini 2.5 Pro</Button>
-        <Button size="icon">
+        <Button size="icon" onClick={handleSend}>
           <Send />
         </Button>
       </div>
---
+++ src/lib/data/create-new.ts
@@ -0,0 +1,38 @@
+import { v7 as uuidv7 } from "uuid";
+import { ChatType, MessageType } from "./types";
+
+function createNewMessage({
+  role,
+  content,
+  name,
+  activeChatId,
+}: {
+  role: "user" | "assistant";
+  content: string;
+  name?: string;
+  activeChatId: string;
+}): MessageType {
+  const newMessage: MessageType = {
+    id: uuidv7(),
+    role,
+    content,
+    name,
+    createdAt: new Date().toISOString(),
+    chatId: activeChatId,
+  };
+
+  return newMessage;
+}
+
+function createNewChat(): ChatType {
+  const newChat: ChatType = {
+    id: uuidv7(),
+    title: "New Chat",
+    createdAt: new Date().toISOString(),
+    lastOpenedAt: new Date().toISOString(),
+  };
+
+  return newChat;
+}
+
+export { createNewMessage, createNewChat };
